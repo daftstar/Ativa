c3a987e29ff40ab10f118ed4b1cea3b9ceb01e56
Nik Daftary <nikdaftary@gmail.com>
2013-10-14 11:31:11 -0500

    moved before_action :authenticate_user! to Application controller to force user login before any actions can be taken (even from index)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+# this allows to allow for name and profile pic to save
+before_action :configure_permitted_parameters, if: :devise_controller?
+#makes sure a user is logged in before any action can be taken
+before_action :authenticate_user!
+
+protected
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.for(:account_update) << :name
+  devise_parameter_sanitizer.for(:account_update) << :profile_pic
+  devise_parameter_sanitizer.for(:sign_up) << :name
+  devise_parameter_sanitizer.for(:sign_up) << :profile_pic
+end
+
+
 end
